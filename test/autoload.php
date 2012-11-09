cb41f6b22515193ec435e7d98fa60d5855de8163
Rostislav Mykhajliw <necromant2005@gmail.com>
2012-11-09 22:22:20 +0200

    travis gearman

--- test/autoload.php
+++ test/autoload.php
@@ -1,5 +1,6 @@
 <?php
-spl_autoload_register(function ($class) {
-    return include 'src/' . $class . '.php';
-});
+#spl_autoload_register(function ($class) {
+#    return include 'src/' . $class . '.php';
+#});
+include_once __DIR__ . '/../src/Ping.php';
 
