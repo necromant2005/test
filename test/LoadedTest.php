afdc05b3a471a9ba7d4badaa88b60d0327c56b73
Rostislav Mykhajliw <necromant2005@gmail.com>
2012-11-09 22:18:01 +0200

    travis gearman

--- test/LoadedTest.php
+++ test/LoadedTest.php
@@ -3,16 +3,16 @@ class LoadedTest extends PHPUnit_Framework_TestCase
 {
     public function testMemcache()
     {
-        $this->assertTrue(extension_loaded('memcache');
+        $this->assertTrue(extension_loaded('memcache'));
     }
 
     public function testMongo()
     {
-        $this->assertTrue(extension_loaded('mongo');
+        $this->assertTrue(extension_loaded('mongo'));
     }
 
     public function testGearman()
     {
-        $this->assertTrue(extension_loaded('gearman');
+        $this->assertTrue(extension_loaded('gearman'));
     }
 }
