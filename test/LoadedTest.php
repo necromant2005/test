213e994a688d976bd0f8d5e371cff647ee99f60a
Rostislav Mykhajliw <necromant2005@gmail.com>
2012-11-09 22:12:53 +0200

    travis gearman

---
+++ test/LoadedTest.php
@@ -0,0 +1,18 @@
+<?php
+class LoadedTest extends PHPUnit_Framework_TestCase
+{
+    public function testMemcache()
+    {
+        $this->assertTrue(extension_loaded('memcache');
+    }
+
+    public function testMongo()
+    {
+        $this->assertTrue(extension_loaded('mongo');
+    }
+
+    public function testGearman()
+    {
+        $this->assertTrue(extension_loaded('gearman');
+    }
+}
